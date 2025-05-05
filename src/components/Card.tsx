61b1470b3191536329d1a51b88ae675c10dfdf56
Manu Morante <manu@estadologico.com>
2025-05-05 21:53:44 +0200

    add shared Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,10 @@
-export default function Card() {
-  return <div>Card</div>
+import clsx from 'clsx'
+
+interface CardProps {
+  children: React.ReactNode
+  className?: string
+}
+
+export default function Card({ children, className }: CardProps) {
+  return <div className={clsx('rounded-lg bg-white p-6 shadow-sm', className)}>{children}</div>
 }
