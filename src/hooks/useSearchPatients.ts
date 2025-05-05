ae9bee1ac5e0b1f31fb76759231cdb3dac076945
Manu Morante <manu@estadologico.com>
2025-05-05 21:05:57 +0200

    refactor: extract useDebounce to hook

--- src/hooks/useSearchPatients.ts
+++ src/hooks/useSearchPatients.ts
@@ -1,16 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { searchPatients } from '@/services/api'
+import { useDebounce } from './useDebounce'
 
 export function useSearchPatients(initialQuery = '', minLength = 3) {
   const [query, setQuery] = useState(initialQuery)
-  const [debouncedQuery, setDebouncedQuery] = useState(initialQuery)
-
-  // Debounce search query
-  useEffect(() => {
-    const timer = setTimeout(() => setDebouncedQuery(query), 300)
-    return () => clearTimeout(timer)
-  }, [query])
+  const debouncedQuery = useDebounce(query)
 
   // Only search if we have enough characters
   const shouldSearch = debouncedQuery.length >= minLength
