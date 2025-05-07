7aa8e12bdad1013acf289b05454c7265de860f80
Manu Morante <manu@estadologico.com>
2025-05-07 18:25:34 +0200

    chore: add @heroicons/react dependency and replace SVG with ArrowLeftIcon in PatientDetailPage

--- src/pages/PatientDetailPage.tsx
+++ src/pages/PatientDetailPage.tsx
@@ -1,5 +1,6 @@
 import { NotesList } from '@/components/notes'
 import { usePatient } from '@/hooks/usePatients'
+import { ArrowLeftIcon } from '@heroicons/react/24/outline'
 import { useNavigate, useParams } from 'react-router-dom'
 
 export default function PatientDetailPage() {
@@ -28,18 +29,7 @@ export default function PatientDetailPage() {
       <button
         onClick={() => navigate(-1)}
         className="button secondary mb-8 flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round">
-          <path d="M19 12H5M12 19l-7-7 7-7" />
-        </svg>
+        <ArrowLeftIcon className="h-5 w-5" />
         Back
       </button>
 
