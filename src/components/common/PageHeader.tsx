9da1854eb8ad90ed0eac3ffdaeb311fbd810db85
Manu Morante <manu@estadologico.com>
2025-05-08 11:31:36 +0200

    refactor: update PageHeader component to toggle navigation icon based on state for improved user interaction

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -1,17 +1,18 @@
-import { Bars3Icon } from '@heroicons/react/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { uiStore } from '@/stores/uiStore'
 
 export default function PageHeader({ title }: { title: string }) {
   const toggleNav = uiStore((state) => state.toggleNav)
+  const isNavOpen = uiStore((state) => state.isNavOpen)
 
   return (
-    <div className="flex items-center gap-1 pt-1 pb-4 sm:pb-8">
+    <div className="flex w-full items-center justify-between gap-1 pt-1 pb-4 sm:pb-8">
+      <h1 className="text-lg font-light sm:text-xl">{title}</h1>
       <button
         onClick={toggleNav}
         className="rounded-md p-1.5 text-gray-500 hover:bg-zinc-100 sm:hidden sm:p-2">
-        <Bars3Icon className="h-5 w-5 sm:h-6 sm:w-6" />
+        {isNavOpen ? <XMarkIcon className="h-6 w-6" /> : <Bars3Icon className="h-6 w-6" />}
       </button>
-      <h1 className="text-lg font-light sm:text-xl">{title}</h1>
     </div>
   )
 }
